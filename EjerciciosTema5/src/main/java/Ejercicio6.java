f4997db2a5a3c264ad6e7b41761458c86a2ade97
Pablo <113892146+PabloRoher@users.noreply.github.com>
2023-02-23 11:20:18 +0000

    Add files via upload

--- EjerciciosTema5/src/main/java/Ejercicio6.java
+++ EjerciciosTema5/src/main/java/Ejercicio6.java
@@ -4,16 +4,23 @@ public class Ejercicio6 {
         int tamano = 4;
         int i,j;
         int tab2d[][] = new int[tamano][tamano];
-
+        int val = 1;
 
         for (j = 0; j < tamano ; j++) {
             for (i = 0; i < tamano ; i++) {
-                tab2d[j][i] = j + 1 + (4 * i);
+                if (i == 0){
+                    tab2d[j][i] = j;
+                    val = j;
+                }else {
+                    val +=  1;
+                    tab2d[j][i] = val;
+
+                }
+
                 System.out.print(tab2d[j][i]);
                 System.out.print(" ");
             }
             System.out.println();
         }
-
     }
 }
