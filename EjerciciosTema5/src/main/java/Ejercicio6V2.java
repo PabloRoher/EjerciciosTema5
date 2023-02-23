3e6cb819b859188d3412a5d3ffc264210d29aec0
Pablo <113892146+PabloRoher@users.noreply.github.com>
2023-02-23 10:36:11 +0000

    Add files via upload

---
+++ EjerciciosTema5/src/main/java/Ejercicio6V2.java
@@ -0,0 +1,19 @@
+public class Ejercicio6V2 {
+    public static void main(String[] args){
+
+        int tamano = 4;
+        int i,j;
+        int tab2d[][] = new int[tamano][tamano];
+        
+
+        for (j = 0; j < tamano ; j++) {
+            for (i = 0; i < tamano ; i++) {
+                tab2d[j][i] = j + 1 + (4 * i);
+                System.out.print(tab2d[j][i]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
