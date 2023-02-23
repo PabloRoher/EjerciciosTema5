e54a83be06d427b4b3d7432011754b40cbbcd772
Pablo <113892146+PabloRoher@users.noreply.github.com>
2023-02-23 10:37:43 +0000

    Add files via upload

--- EjerciciosTema5/src/main/java/Ejercicio6.java
+++ EjerciciosTema5/src/main/java/Ejercicio6.java
@@ -1,28 +1,16 @@
 public class Ejercicio6 {
     public static void main(String[] args){
-        /*
-        Constante TAMAÑO       : entero <- 3
-        Variable i, j, val: entero
-        Variable tab2d: entero[TAMAÑO][TAMAÑO]
-        Inicio
-        val <- 1
-        Para j <- 0 a TAMAÑO - 1
-        Para i <- 0 a TAMAÑO - 1
-        tab2d[j][i] <- val
-        val <- val + 1
 
-         */
-
-        int tamano = 3;
+        int tamano = 4;
         int i,j;
         int tab2d[][] = new int[tamano][tamano];
-        int val = 1;
 
-        for (j = 0; j < tamano - 1; j++) {
-            for (i = 0; i < tamano - 1; i++) {
-                tab2d[j][i] = val;
-                val = val + 1;
+
+        for (j = 0; j < tamano ; j++) {
+            for (i = 0; i < tamano ; i++) {
+                tab2d[j][i] = j + 1 + (4 * i);
                 System.out.print(tab2d[j][i]);
+                System.out.print(" ");
             }
             System.out.println();
         }
